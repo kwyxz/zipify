d85aa1dd9126ed17ad3135a1231b93e2c0df8ea4
kwyxz <kwyxz@kwyxz.org>
2018-08-15 18:56:22 -0700

    Much better take on this

--- zipify.sh
+++ zipify.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
+# the working directory
 PWD=$(pwd)
 
+# if error, exit 1
 _die ()
 {
   echo "Error: $1"
   exit 1
 }
 
+# find the necessary binaries, unzip and 7z must be in $PATH
 _getbin ()
 {
   if ! UNZIP=$(which unzip); then
@@ -17,30 +20,36 @@ _getbin ()
   fi
 }
 
+# extract zip contents and then add them to 7z archive
+_zipify ()
+{
+  echo ======== $1 ========
+  SZFILE=$(basename $1 .zip).7z
+  mkdir $1.dir && cd $1.dir
+  $UNZIP ../$1 && $SZIP a $SZFILE ./*
+  mv $SZFILE/.. && cd .. && rm -rf $1.dir
+  if [ -f ./$SZFILE ]; then
+    rm $1
+  fi
+}
+
+# main loop
 if [ $# -lt 1 ]; then
   _die "no argument given, must be file or directory"
 fi
 
-_getbin
-
-while [ $# -ne 0 ]
-do
-  cd $1
-    for zipfile in *.zip; do
-      echo ======== $zipfile ========
-      SZFILE=$(basename $zipfile .zip)
-      mkdir $SZFILE
-      cd $SZFILE
-      unzip ../$zipfile
-      7z a $SZFILE.7z ./*
-      cd ..
-      mv $SZFILE/$SZFILE.7z .
-      rm -rf $SZFILE
-      if [ -f ./$SZFILE.7z ]; then
-        rm $zipfile
-      fi
-    done
-  cd $PWD
+while [ $# -ne 0 ]; do
+  if [ -d $1 ]; then
+    cd $1
+      for ZIPFILE in *.zip; do
+        _zipify $ZIPFILE
+      done
+    cd $PWD
+  elif [ -f $1 ]; then
+    _zipify $1
+  else
+    _die "$1 does not exist"
+  fi
   shift
 done
 
