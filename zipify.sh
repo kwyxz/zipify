09e97cdc1b1263f511ba77758b91c7471a2b0ee9
Benjamin FRANCOIS <kwyxz@kwyxz.org>
2020-01-11 19:46:06 -0800

    Allow 7z to zip conversion too

--- zipify.sh
+++ zipify.sh
@@ -18,10 +18,12 @@ _skip ()
 # find the necessary binaries, unzip and 7z must be in $PATH
 _getbin ()
 {
-  if ! UNZIP=$(which unzip); then
+  if ! UNZIP=$(command -v unzip); then
     _die "unzip binary not found in \$PATH"
-  elif ! SZIP=$(which 7z); then
+  elif ! SZIP=$(command -v 7z); then
     _die "7z binary not found in \$PATH"
+  elif ! ZIP=$(command -v zip); then
+    _die "zip binary not found in \$PATH"
   fi
 }
 
@@ -30,17 +32,27 @@ _zipify ()
 {
   echo ======== "$1" ========
   if $(file -i "$1" | grep -q 'application/zip'); then
-    SZFILE=$(basename "$1" .zip).7z
+    NEWFILE=$(basename "$1" .zip).7z
     mkdir "$1.dir" && cd "$1.dir"
     $UNZIP -q ../"$1"
-    $SZIP a -sdel "$SZFILE" ./*
-    mv "$SZFILE" .. 
+    $SZIP a -sdel "$NEWFILE" ./*
+    mv "$NEWFILE" ..
     cd .. && rmdir "$1.dir"
-    if [ -f ./"$SZFILE" ]; then
+    if [ -f ./"$NEWFILE" ]; then
+      rm "$1"
+    fi
+  elif $(file -i "$1" | grep -q 'application/x-7z-compressed'); then
+    NEWFILE=$(basename "$1" .7z).zip
+    mkdir "$1.dir" && cd "$1.dir"
+    $SZIP x ../"$1"
+    $ZIP -r "$NEWFILE" ./*
+    mv "$NEWFILE" ..
+    cd .. && rmdir "$1.dir"
+    if [ -f ./"$NEWFILE" ]; then
       rm "$1"
     fi
   else
-    _skip "$1 is not a zip file"
+    _skip "$1 is not a zip nor a 7z file"
   fi
 }
 
